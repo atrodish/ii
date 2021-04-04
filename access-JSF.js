33136bcedab538a19c814da04dbca0eac1817740
atrodish <81901908+atrodish@users.noreply.github.com>
2021-04-04 14:02:34 +0530

    Add files via upload

---
+++ access-JSF.js
@@ -0,0 +1,18 @@
+var iiAppRes = {
+    js:{
+        whammy : 'https://invitationindia.netlify.app/app-js/whammy.min.js',
+        translation : 'https://invitationindia.netlify.app/app-js/translation.min.js',
+        renderImg : 'https://invitationindia.netlify.app/app-js/render-image.min.js',
+        ffmpeg : 'https://invitationindia.netlify.app/app-js/ffmpeg.min.js',
+        iiAppHTML : 'https://invitationindia.netlify.app/app-js/editorAppHTML-Ver2.js',
+        createVideo : 'https://invitationindia.netlify.app/app-js/create-video.js',
+        convertToMp4 : 'https://invitationindia.netlify.app/app-js/convert-to-mp4.js',
+        cardWithPhotos : 'https://invitationindia.netlify.app/app-js/card-with-photos.js',
+        appLangSpecificFontList : 'https://invitationindia.netlify.app/app-js/appLangSpecificFontList-iiv2.js'
+    }
+}
+
+function iigetAppRes(){
+ return iiAppRes;
+}
+
